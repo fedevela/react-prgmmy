794ce92b992c140fa0d7d11831785f3c264e53d8
Federico Vela <fedevela@gmail.com>
2023-02-11 02:31:52 -0500

    with creation

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 import Draggable from "react-draggable";
+import feathersClient from "./feathersClient";
+
+const xycoordsService = feathersClient.service("xycoords");
 
 function App() {
+  // const onDrag = (event, ui) => {
+  //   // console.log(event);
+  //   console.log(`(${ui.x},${ui.y})`);
+  //   // this.setState({
+  //   //   deltaPosition: {
+  //   //     x: x + ui.deltaX,
+  //   //     y: y + ui.deltaY,
+  //   //   },
+  //   // });
+  // };
+
+  // const onStart = () => {
+  //   console.log(`START`);
+  // };
+  // const onStop = () => {
+  //   console.log(`STOP`);
+  // };
+
   const onDrag = (event, ui) => {
-    // console.log(event);
-    console.log(`(${ui.x},${ui.y})`);
-    // this.setState({
-    //   deltaPosition: {
-    //     x: x + ui.deltaX,
-    //     y: y + ui.deltaY,
-    //   },
-    // });
-  };
-
-
-  const onStart = () => {
-    console.log(`START`)
-  };
-  const onStop = () => {
-    console.log(`STOP`)
+    const { x, y } = ui;
+    xycoordsService.create({ x, y }).then((wut) => {
+      console.log(wut);
+    });
   };
 
   return (
     <div>
-      <Draggable onDrag={onDrag} onStart={onStart} onStop={onStop}>
+      {/* <Draggable onDrag={onDrag} onStart={onStart} onStop={onStop}>÷ */}
+      <Draggable onDrag={onDrag}>
         <div className="box">Progummy</div>
       </Draggable>
     </div>
