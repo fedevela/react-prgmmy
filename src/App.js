f6c65ce17316d4c02d79e064f77519d980bfe9fc
Federico Vela <fedevela@gmail.com>
2023-02-11 03:56:08 -0500

    with basic css drag

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
-import Draggable from "react-draggable";
+// import Draggable from "react-draggable";
 import feathersClient from "./feathersClient";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useEffect, useState } from "react";
 
 const xycoordsService = feathersClient.service("xycoords");
 
@@ -25,43 +25,105 @@ function App() {
   // };
   const [getX, setX] = useState(0);
   const [getY, setY] = useState(0);
-  useEffect(() => {
-    xycoordsService
-      .find({
-        query: {
-          $limit: 1,
-          $sort: {
-            createdAt: -1,
-          },
-        },
-      })
-      .then((startCoords) => {
-        const { x, y } = startCoords.data[0];
-        setX(x);
-        setY(y);
-      });
-  }, []);
+  // useEffect(() => {
+  //   xycoordsService
+  //     .find({
+  //       query: {
+  //         $limit: 1,
+  //         $sort: {
+  //           createdAt: -1,
+  //         },
+  //       },
+  //     })
+  //     .then((startCoords) => {
+  //       const { x, y } = startCoords.data[0];
+  //       setX(x);
+  //       setY(y);
+  //     });
+  // }, []);
+  const elemRef = useRef(null);
+  // const dragProps = useRef(null);
+  const [getDragProps, setDragProps] = useState({});
 
-  const onDrag = (event, ui) => {
-    const { x, y } = ui;
-    xycoordsService.create({ x, y }).then((createdCoords) => {
-      console.log(createdCoords);
+  const [getIsDragging, setIsDragging] = useState(false);
+
+  const onMouseDown = (event) => {
+    console.log("MOUSE DOWN");
+    // console.log(event);
+    setIsDragging(true);
+    const { target, clientX, clientY } = event;
+    const { offsetTop, offsetLeft } = target;
+    const { left, top } = elemRef.current.getBoundingClientRect();
+
+    setDragProps({
+      dragStartLeft: left - offsetLeft,
+      dragStartTop: top - offsetTop,
+      dragStartX: clientX,
+      dragStartY: clientY,
     });
+    // window.addEventListener("mousemove", doDragging, false);
+    // window.addEventListener("mouseup", stopDragging, false);
   };
 
+  const doDragging = (event) => {
+    if (getIsDragging) {
+      elemRef.current.style.transform = `translate(${
+        getDragProps.dragStartLeft + event.clientX - getDragProps.dragStartX
+      }px, ${
+        getDragProps.dragStartTop + event.clientY - getDragProps.dragStartY
+      }px)`;
+      // console.log("MOUSE DRAG");
+      console.log(`(${event.clientX},${event.clientY})`);
+    }
+  };
+
+  const stopDragging = (event) => {
+    console.log("MOUSE UP");
+    // console.log(event);
+    setIsDragging(false);
+
+    // window.removeEventListener("mousemove", doDragging, false);
+    // window.removeEventListener("mouseup", stopDragging, false);
+  };
+
+  // const onDrag = (event, ui) => {
+  //   const { x, y } = ui;
+  //   xycoordsService.create({ x, y }).then((createdCoords) => {
+  //     console.log(createdCoords);
+  //   });
+
+  //   // event.preventDefault();
+  //   event.stopPropagation();
+  // };
+
   return (
-    <div>
-      {/* <Draggable onDrag={onDrag} onStart={onStart} onStop={onStop}>÷ */}
-      <Draggable
-        onDrag={onDrag}
-        position={{x: getX, y: getY}}
-      >
-        <div className="box">Progummy</div>
-      </Draggable>
-      <div>
-        {getX},{getY}
-      </div>
+    <div
+      onMouseDown={onMouseDown}
+      onMouseMove={doDragging}
+      onMouseUp={stopDragging}
+      ref={elemRef}
+      className="box"
+    >
+      Progummy
     </div>
+    // <Draggable
+    //     onDrag={onDrag}>
+    // <div>
+    //   <h1>I can now be moved around!</h1>
+    // </div>
+    // </Draggable>
+    // <div>
+    //   {/* <Draggable onDrag={onDrag} onStart={onStart} onStop={onStop}>÷ */}
+    //   <Draggable
+    //     // onDrag={onDrag}
+    //     // position={{x: getX, y: getY}}
+    //   >
+    //     <div className="box">Progummy</div>
+    //   </Draggable>
+    //   <div>
+    //     {getX},{getY}
+    //   </div>
+    // </div>
   );
 }
 
