58dca1ad8d384696ee82b9a9cacc48b34b4cc578
Federico Vela <fedevela@gmail.com>
2023-02-11 13:38:52 -0500

    first release

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
 import feathersClient from "./feathersClient";
 import { useRef, useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const xycoordsService = feathersClient.service("xycoords");
+const clientUUID = uuidv4();
 
 function App() {
   const draggableElemRef = useRef(null);
   const [getDragStartProps, setDragStartProps] = useState({});
   const [getIsDragging, setIsDragging] = useState(false);
+
+  function executeCSSTranslate(xValue, yValue) {
+    draggableElemRef.current.style.transform = `translate(${xValue}px, ${yValue}px)`;
+  }
 
   useEffect(() => {
     xycoordsService
@@ -21,9 +27,18 @@ function App() {
       })
       .then((startCoords) => {
         const { xValue, yValue } = startCoords.data[0];
-        draggableElemRef.current.style.transform = `translate(${xValue}px, ${yValue}px)`;
+        executeCSSTranslate(xValue, yValue);
         draggableElemRef.current.style.visibility = "visible";
       });
+
+    xycoordsService.on("created", (coord) => {
+      if (clientUUID !== coord.clientUUID) {
+        const { xValue, yValue } = coord;
+        // const { left, top } = draggableElemRef.current.getBoundingClientRect();
+        executeCSSTranslate(xValue, yValue);
+        // debugger;
+      }
+    });
   }, []);
 
   function executeUpdatePosition(clientX, clientY) {
@@ -31,13 +46,12 @@ function App() {
       getDragStartProps.dragStartLeft + clientX - getDragStartProps.dragStartX;
     const yValue =
       getDragStartProps.dragStartTop + clientY - getDragStartProps.dragStartY;
-    draggableElemRef.current.style.transform = `translate(${xValue}px, ${yValue}px)`;
+    executeCSSTranslate(xValue, yValue);
 
-    xycoordsService
-      .create({ xValue, yValue })
-      .catch((error) => {
-        console.error(error);
-      });
+    xycoordsService.create({ xValue, yValue, clientUUID }).catch((error) => {
+      console.error("Error creating coordinates!");
+      console.error(error);
+    });
   }
 
   const startDragging = (event) => {
@@ -61,7 +75,7 @@ function App() {
     }
   };
 
-  const stopDragging = (event) => {
+  const stopDragging = () => {
     setIsDragging(false);
   };
 
